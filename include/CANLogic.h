cbf8fa411cd819b247d33a9b24eaed94682caad7
GrKoR <dr.g.kornev@gmail.com>
2023-05-09 16:45:27 +0400

    upd: CANLogic refactoring

--- include/CANLogic.h
+++ include/CANLogic.h
@@ -10,21 +10,29 @@ extern UART_HandleTypeDef huart1;
 
 namespace CANLib
 {
-	/* Настройки */
-	static constexpr uint8_t CFG_CANObjects = 6;	// Кол-во объектов CAN.
-	/* */
-	
+	//*********************************************************************
+	// CAN Library settings
+	//*********************************************************************
+
+	/// @brief Number of CANObjects in CANManager
+	static constexpr uint8_t CFG_CANObjectsCount = 12;
+
+	/// @brief The size of CANManager's internal CAN frame buffer
+	static constexpr uint8_t CFG_CANFrameBufferSize = 16;
+	//*********************************************************************
+	//*********************************************************************
+
 	// structure for all data fields of CANObject
-	rear_light_can_data_t light_ecu_can_data;
-	
-	CANManager<CFG_CANObjects> can_manager(&HAL_CAN_Send);
-	
+	// rear_light_can_data_t light_ecu_can_data;
+
+	CANManager<CFG_CANObjectsCount, CFG_CANFrameBufferSize> can_manager(&HAL_CAN_Send);
+
 	// common blocks
 	CANObject<uint8_t, 7> obj_block_info(REAR_LIGHT_CANO_ID_BLOCK_INFO, 15000, 300);
 	CANObject<uint8_t, 7> obj_block_health(REAR_LIGHT_CANO_ID_BLOCK_HEALTH, CAN_TIMER_DISABLED, 300);
 	CANObject<uint8_t, 7> obj_block_cfg(REAR_LIGHT_CANO_ID_BLOCK_CFG, CAN_TIMER_DISABLED, CAN_ERROR_DISABLED);
 	CANObject<uint8_t, 7> obj_block_error(REAR_LIGHT_CANO_ID_BLOCK_ERROR, CAN_TIMER_DISABLED, 300);
-	
+
 	// specific blocks
 	CANObject<uint8_t, 1> obj_side_beam(REAR_LIGHT_CANO_ID_SIDE_BEAM, CAN_TIMER_DISABLED, 300);
 	CANObject<uint8_t, 1> obj_brake_light(REAR_LIGHT_CANO_ID_BRAKE_LIGHT, CAN_TIMER_DISABLED, 300);
@@ -34,231 +42,202 @@ namespace CANLib
 	CANObject<uint8_t, 1> obj_hazard_beam(REAR_LIGHT_CANO_ID_HAZARD_BEAM, CAN_TIMER_DISABLED, 300);
 	CANObject<uint8_t, 1> obj_custom_beam(REAR_LIGHT_CANO_ID_CUSTOM_BEAM, CAN_TIMER_DISABLED, 300);
 	CANObject<uint8_t, 1> obj_custom_image(REAR_LIGHT_CANO_ID_CUSTOM_IMAGE, CAN_TIMER_DISABLED, 300);
-	
-	
+
 	// вызывается, если по CAN пришла команда включения/выключения габаритов
 	void side_beam_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		/*
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
-		*/
+		// light_ecu_can_data.side_beam.brightness = can_frame.data[0];
 
-		can_frame.initialized = true;
-		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
-		can_frame.raw_data_length = 1;
-
-		light_ecu_can_data.side_beam.brightness = can_frame.data[0];
-
-		if (light_ecu_can_data.side_beam.brightness == 0)
+		// if (light_ecu_can_data.side_beam.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
-			//OUT1_OFF;
 			Matrix::matrixObj.HideLayer(2);
 			PowerOut::outObj.SetOff(3);
 		}
 		else
 		{
-			//OUT1_ON;
 			Matrix::matrixObj.ShowLayer(2);
 			PowerOut::outObj.SetOn(3);
-			// TODO: установка яркости не корректна, так как задаётся яркость всей панели, а не только одних огней
-			// matrixObj.SetBrightness(light_ecu_can_data.side_beam.brightness);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
 
 	// вызывается, если по CAN пришла команда включения/выключения стоп-сигналов
 	void brake_light_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		/*
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
-		*/
+		// light_ecu_can_data.brake_light.brightness = can_frame.data[0];
 
-		can_frame.initialized = true;
-		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
-		can_frame.raw_data_length = 2;
-
-		light_ecu_can_data.brake_light.brightness = can_frame.data[0];
-
-		if (light_ecu_can_data.brake_light.brightness == 0)
+		// if (light_ecu_can_data.brake_light.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
-			//OUT2_OFF;
 			Matrix::matrixObj.HideLayer(4);
 			PowerOut::outObj.SetOff(2);
 		}
 		else
 		{
-			//OUT2_ON;
 			Matrix::matrixObj.ShowLayer(4);
 			PowerOut::outObj.SetOn(2);
-			// TODO: установка яркости не корректна, так как задаётся яркость всей панели, а не только одних огней
-			// matrixObj.SetBrightness(light_ecu_can_data.brake_light.brightness);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
 
 	// вызывается, если по CAN пришла команда включения/выключения заднего хода
 	void reverse_light_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
+		// light_ecu_can_data.reverse_light.brightness = can_frame.data[0];
 
-		light_ecu_can_data.reverse_light.brightness = can_frame.data[0];
-
-		if (light_ecu_can_data.reverse_light.brightness == 0)
+		// if (light_ecu_can_data.reverse_light.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
-			//OUT3_OFF;
 			Matrix::matrixObj.HideLayer(3);
+			// PowerOut::outObj.SetOff(X);
 		}
 		else
 		{
-			//OUT3_ON;
 			Matrix::matrixObj.ShowLayer(3);
-			// TODO: установка яркости не корректна, так как задаётся яркость всей панели, а не только одних огней
-			// matrix.SetBrightness(light_ecu_can_data.reverse_light.brightness);
+			// PowerOut::outObj.SetOn(X);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
 
 	// вызывается, если по CAN пришла команда включения/выключения левого поворотника
 	void turn_left_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
-
-		light_ecu_can_data.left_indicator.brightness = can_frame.data[0];
+		// light_ecu_can_data.left_indicator.brightness = can_frame.data[0];
 
 		// TODO: надо же ещё мигать выводами OUTx?
-		if (light_ecu_can_data.left_indicator.brightness == 0)
+
+		// if (light_ecu_can_data.left_indicator.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
-			//OUT4_OFF;
 			Matrix::matrixObj.HideLayer(5);
+			// PowerOut::outObj.SetOff(X);
 		}
 		else
 		{
-			//OUT4_ON;
 			Matrix::matrixObj.ShowLayer(5);
-			// TODO: установка яркости не корректна, так как задаётся яркость всей панели, а не только одних огней
-			// matrixObj.SetBrightness(light_ecu_can_data.left_indicator.brightness);
+			// PowerOut::outObj.SetOn(X);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
 
 	// вызывается, если по CAN пришла команда включения/выключения правкого поворотника
 	void turn_right_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
-
-		light_ecu_can_data.right_indicator.brightness = can_frame.data[0];
+		// light_ecu_can_data.right_indicator.brightness = can_frame.data[0];
 
 		// TODO: надо же ещё мигать выводами OUTx?
-		if (light_ecu_can_data.right_indicator.brightness == 0)
+
+		// if (light_ecu_can_data.right_indicator.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
-			//OUT5_OFF;
 			Matrix::matrixObj.HideLayer(6);
+			// PowerOut::outObj.SetOff(X);
 		}
 		else
 		{
-			//OUT5_ON;
 			Matrix::matrixObj.ShowLayer(6);
-			// TODO: установка яркости не корректна, так как задаётся яркость всей панели, а не только одних огней
-			// matrixObj.SetBrightness(light_ecu_can_data.right_indicator.brightness);
+			// PowerOut::outObj.SetOn(X);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
 
 	// вызывается, если по CAN пришла команда включения/выключения аварийного сигнала
 	void hazard_beam_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
-
-		light_ecu_can_data.hazard_beam.brightness = can_frame.data[0];
+		// light_ecu_can_data.hazard_beam.brightness = can_frame.data[0];
 
 		// TODO: надо же ещё мигать выводами OUTx?
-		if (light_ecu_can_data.hazard_beam.brightness == 0)
+
+		// if (light_ecu_can_data.hazard_beam.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
-			//OUT4_OFF;
-			//OUT5_OFF;
 			Matrix::matrixObj.HideLayer(7);
+			// PowerOut::outObj.SetOff(X);
 		}
 		else
 		{
-			//OUT4_ON;
-			//OUT5_ON;
 			Matrix::matrixObj.ShowLayer(7);
-			// TODO: установка яркости не корректна, так как задаётся яркость всей панели, а не только одних огней
-			// matrixObj.SetBrightness(light_ecu_can_data.hazard_beam.brightness);
+			// PowerOut::outObj.SetOn(X);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
 
 	// вызывается, если по CAN пришла команда включения/выключения пользовательского света
 	void custom_beam_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
+		// light_ecu_can_data.custom_beam.brightness = can_frame.data[0];
 
-		light_ecu_can_data.custom_beam.brightness = can_frame.data[0];
-
-		if (light_ecu_can_data.custom_beam.brightness == 0)
+		// if (light_ecu_can_data.custom_beam.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
-			//OUT6_OFF;
+			// PowerOut::outObj.SetOff(X);
 		}
 		else
 		{
-			//OUT6_ON;
-			// TODO: включение - это любое значение больше 0?
-			// Или жестко 255 будет включение, а 1..254 не будут влиять ни на что?
+			// PowerOut::outObj.SetOn(X);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
 
 	// вызывается, если по CAN пришла команда включения/выключения пользовательского изображения на панели
 	void custom_image_set_handler(can_frame_t &can_frame, can_error_t &error)
 	{
-		can_frame.initialized = false;
-		error.error_section = ERROR_SECTION_CAN_OBJECT;
-		error.error_code = ERROR_CODE_OBJECT_SET_FUNCTION_IS_MISSING;
-		error.function_id = CAN_FUNC_SET_OUT_ERR;
-		return;
-
-		light_ecu_can_data.custom_image.brightness = can_frame.data[0];
+		// light_ecu_can_data.custom_image.brightness = can_frame.data[0];
 
 		// TODO: надо же ещё мигать выводами OUTx?
-		if (light_ecu_can_data.custom_image.brightness == 0)
+
+		// if (light_ecu_can_data.custom_image.brightness == 0)
+		if (can_frame.data[0] == 0)
 		{
 			Matrix::matrixObj.HideLayer(1);
+			// PowerOut::outObj.SetOff(X);
 		}
 		else
 		{
 			Matrix::matrixObj.ShowLayer(1);
-			// TODO: установка яркости не корректна, так как задаётся яркость всей панели, а не только одних огней
-			// matrixObj.SetBrightness(light_ecu_can_data.custom_image.brightness);
+			// PowerOut::outObj.SetOn(X);
 		}
+
+		can_frame.initialized = true;
+		can_frame.function_id = CAN_FUNC_SET_OUT_OK;
+		// can_frame.data[0] doesn't change
+		can_frame.raw_data_length = 2;
 	}
-	
+
 	inline void Setup()
 	{
 		// set CAN data structure to zero
-		memset(&light_ecu_can_data, 0, sizeof(light_ecu_can_data));
-		
+		// memset(&light_ecu_can_data, 0, sizeof(light_ecu_can_data));
+
 		obj_side_beam.RegisterFunctionSet(&side_beam_set_handler);
 		obj_brake_light.RegisterFunctionSet(&brake_light_set_handler);
 		obj_reverse_light.RegisterFunctionSet(&reverse_light_set_handler);
@@ -267,13 +246,13 @@ namespace CANLib
 		obj_hazard_beam.RegisterFunctionSet(&hazard_beam_set_handler);
 		obj_custom_beam.RegisterFunctionSet(&custom_beam_set_handler);
 		obj_custom_image.RegisterFunctionSet(&custom_image_set_handler);
-		
+
 		// common blocks
 		can_manager.RegisterObject(obj_block_info);
 		can_manager.RegisterObject(obj_block_health);
 		can_manager.RegisterObject(obj_block_cfg);
 		can_manager.RegisterObject(obj_block_error);
-		
+
 		// specific blocks
 		can_manager.RegisterObject(obj_side_beam);
 		can_manager.RegisterObject(obj_brake_light);
@@ -283,31 +262,37 @@ namespace CANLib
 		can_manager.RegisterObject(obj_hazard_beam);
 		can_manager.RegisterObject(obj_custom_beam);
 		can_manager.RegisterObject(obj_custom_image);
-		
-		// Tests
+
+		//*************************************************************
+		// TODO: CANManager experiments
+		// init normal timer for Block_Info
 		obj_block_info.SetValue(0, 0x66, CAN_TIMER_TYPE_NORMAL);
-		
+		//*************************************************************
+
 		return;
 	}
-	
+
 	inline void Loop(uint32_t &current_time)
 	{
 		can_manager.Process(current_time);
 
+		//*************************************************************
+		// TODO: CANManager experiments
 		static uint32_t iter = 0;
-		if(current_time - iter > 1000)
+		if (current_time - iter > 1000)
 		{
 			iter = current_time;
-			
-			uint8_t *data = (uint8_t*)&current_time;
+
+			uint8_t *data = (uint8_t *)&current_time;
 			obj_block_info.SetValue(3, data[0], CAN_TIMER_TYPE_NORMAL);
 			obj_block_info.SetValue(4, data[1], CAN_TIMER_TYPE_NORMAL);
 			obj_block_info.SetValue(5, data[2], CAN_TIMER_TYPE_NORMAL);
 			obj_block_info.SetValue(6, data[3], CAN_TIMER_TYPE_NORMAL);
 		}
-		
+		//*************************************************************
+
 		current_time = HAL_GetTick();
-		
+
 		return;
 	}
 }
